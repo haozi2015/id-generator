cc944426483b8c6b919217a14fecf1ca71bdb709
haozi <haozi2015@aliyun.com>
2020-05-28 18:37:15 +0800

    update sql

--- docs/initial/idgenerator.sql
+++ docs/initial/idgenerator.sql
@@ -24,8 +24,8 @@ CREATE TABLE `t_sequence_rule` (
   `digits` tinyint(4) NOT NULL DEFAULT '0' COMMENT '位数，位数不足补0（小于等于0不补）',
   `reset_rule` varchar(20) NOT NULL DEFAULT '' COMMENT '复位规则',
   `status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '状态(0:暂停,1:启用)',
+  `last_update_time` datetime NOT NULL COMMENT '最后更新时间',
+  `initial_value` int(11) NOT NULL COMMENT '初始值',
   PRIMARY KEY (`id`),
   UNIQUE KEY `uniq_key` (`key`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='ID生成规则表';
-
-SET FOREIGN_KEY_CHECKS = 1;
